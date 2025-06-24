edef7e802a9f2588dd33285e99e871da888b7b11
Ygor Castor <ygorcastor@gmail.com>
2025-06-24 15:10:42 +0200

    fix: Fixing EXLA mess
    Release-As: 0.1.1

--- mix.exs
+++ mix.exs
@@ -37,18 +37,16 @@ defmodule ExMemvid.MixProject do
       {:briefly, "~> 0.5", only: :test},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:emlx, github: "elixir-nx/emlx", branch: "main", only: [:test, :dev]},
       {:evision, "~> 0.2"},
       {:ex_check, "~> 0.16", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:exla, "~> 0.10", only: [:dev, :test]},
       {:gen_state_machine, "~> 3.0"},
       {:hnswlib, "~> 0.1"},
       {:mimic, "~> 1.12", only: :test},
       {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:nimble_options, "~> 1.1"},
-      {:nx, "~> 0.10", override: true},
+      {:nx, "~> 0.9"},
       {:qr_code, "~> 3.2"},
       {:qrex, "~> 0.1"},
       {:recode, "~> 0.6", only: :dev, runtime: false},
@@ -56,6 +54,25 @@ defmodule ExMemvid.MixProject do
       {:text_chunker, "~> 0.3"},
       {:xav, "~> 0.10"}
     ]
+    |> maybe_add_exla()
+    |> maybe_add_emlx()
+  end
+
+  ## Exla 0.9 is not building on MacOS, 0.10 is, but bumblebee requires 0.9
+  defp maybe_add_exla(deps) do
+    if System.get_env("USE_EXLA") do
+      deps ++ [{:exla, ">= 0.0.0", only: [:dev, :test]}]
+    else
+      deps
+    end
+  end
+
+  defp maybe_add_emlx(deps) do
+    if System.get_env("USE_EMLX") do
+      deps ++ [{:emlx, github: "elixir-nx/emlx", branch: "main", only: [:test, :dev]}]
+    else
+      deps
+    end
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
